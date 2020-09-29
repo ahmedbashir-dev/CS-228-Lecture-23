6112f6d25969b345f7ced5c3f0a149c04a0d4b30
ahmedbashir-repo <ahmedbashir.w@gmail.com>
2020-09-29 14:53:31 +0500

    Retrieving records from DB and dynamic rendering

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+    <!-- Grey with black text -->
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Add User</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="view_users.php">View Users</a>
+    </li>
+  </ul>
+</nav>
